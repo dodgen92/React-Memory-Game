e80bf6674b7b25b50cd4f040813967bb476383e2
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-05-03 19:39:46 -0400

    Created footer component

---
+++ memorygame/src/components/Footer/Index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Click 20 in a row to win! <img alt="react" src="assets/images/react.svg" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
