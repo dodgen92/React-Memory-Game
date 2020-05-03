62e67a0c340872cdcdb0cdd1658e0484d0ac7e82
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-05-03 19:36:00 -0400

    created header explaining rules and initial file structure

---
+++ memorygame/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Rules:</h1>
+      <h2>Click on an image to earn a point, click on the same image twice you lose.</h2>
+    </header>
+  );
+}
+
+export default Header;
