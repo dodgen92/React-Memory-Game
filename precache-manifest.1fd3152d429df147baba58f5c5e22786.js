c67f6dbf4bb6a45f1840bc3c8f61a696bcee0fd3
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-05-09 12:18:39 -0400

    Updates

---
+++ precache-manifest.1fd3152d429df147baba58f5c5e22786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c095451ae5a73f1cefbcc71134b3df5",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "62ec35f6b805015c1818",
+    "url": "/React-Memory-Game/static/css/main.559ca08b.chunk.css"
+  },
+  {
+    "revision": "a7ee9c55de03050652a2",
+    "url": "/React-Memory-Game/static/js/2.8d5cc82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Memory-Game/static/js/2.8d5cc82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ec35f6b805015c1818",
+    "url": "/React-Memory-Game/static/js/main.361085dc.chunk.js"
+  },
+  {
+    "revision": "a96029dc7a8286f29e4b",
+    "url": "/React-Memory-Game/static/js/runtime-main.de414468.js"
+  }
+]);
